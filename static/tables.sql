2a1275b920e0ff9535df677229b98b0f702628ff
红色贝鱼 <lvrenkun@gmail.com>
2012-10-26 18:25:40 +0800

    static file，templates
    tables init，static file（css,img) html

---
+++ static/tables.sql
@@ -0,0 +1,29 @@
+
+create database family;
+
+use family;
+
+create table member(
+	id 			int(11) 		NOT NULL auto_increment primary key, 
+	name		varchar(100)	NOT NULL 			   ,--姓名
+	create_time	date        						   ,--添加时间	
+	idDelete	int   								   ,--是否被删除
+	field1		varchar(100)						   ,--冗余字段
+	field2		varchar(100)						   ,--冗余字段
+	field3		varchar(100)						    --冗余字段
+)
+
+create table  fee_record(
+	id  			int(11)			NOT NULL auto_increment primary key,
+	fee        	 	DECIMAL(5,2)	NOT null 			   ,---消费金额	
+	FEE_TYPE		INT(3)								   ,---消费类型	
+	DISCRIPTION		varchar(500)	                       ,---消费描述
+	member_ID		INT(11)								   ,---支付成员ID
+	consume_time	date  								   ,---消费时间
+	create_time		date    							   ,---条目创建时间(入库时间)
+	isFinish		int(2)								   ,--是否已经结算过	
+	field1			varchar(100)						   ,---冗余字段
+	field2			varchar(100)						   ,---冗余字段
+	field3			varchar(100)							--冗余字段	
+
+)
