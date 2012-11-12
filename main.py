cbf023b74bdbe2602593827c498d8dbaff8abe0d
红色贝鱼 <lvrenkun@gmail.com>
2012-11-12 18:29:11 +0800

    添加查询功能

--- main.py
+++ main.py
@@ -2,15 +2,20 @@
 # coding:utf-8
 #-*- coding: utf-8 -*-
 import sys,web,json
-from   datetime    import datetime
+from   datetime    import datetime,date
 from   config  	   import config
 from   config.urls import urls
+from   decimal 	   import Decimal
 
 db 	     = config.db
 render   = config.render
 tb 	 	 = 'fee_record'
 tbmember = 'member'
-
+def dthandle(obj):
+	if isinstance(obj,date):
+		return obj.isoformat()
+	elif isinstance(obj,Decimal):
+	    return str(obj) 
 def get_by_id(id):
 	result	= db.select(tb,where='id=$id',vars=locals())
 	if not result:
@@ -23,6 +28,11 @@ class index:
 		sql = 'select consume_time,fee,discription,name from fee_record a left join member b on a.member_id=b.id order by a.consume_time desc'
 		bills = db.query(sql)
 		return render.index(bills)
+	def POST(self):
+		sql = 'select consume_time,fee,discription,name from fee_record a left join member b on a.member_id=b.id order by a.consume_time desc'
+		bills = db.query(sql).list()
+		web.header('Content-Type','application/json')
+		return json.dumps(bills,default=dthandle)
 class new:
 	"""add new bill """
 	def POST(self):
@@ -38,10 +48,9 @@ class newMember:
 class allMember:
 	"""docstring for allMember"""
 	def POST(self):
-		members = db.query("select id,name from member order by name").list()
+		members = db.query("select id,name,create_time from member order by name").list()
 		web.header('Content-Type','application/json')
-		print members
-		return json.dumps(members)
+		return json.dumps(members,default=dthandle)
 	def GET(self):
 		return self.POST()
 class searchBill:
--- static/js/index.js
+++ static/js/index.js
@@ -15,17 +15,21 @@
         	collapsible: true,
         	heightStyle: "content"
         });
+
+
       	$.post( "/allMember" , {
     	},
     	function(result) {
     		if(result){
-    			for(var i=0;i<result.length;i++){
-    				obj = result[i]
+                $('#member_search').append('<option value=0>全部</option>');
+                for(var i=0;i<result.length;i++){
+                    obj = result[i]
     				option = '<option value='+obj.id+'>'+obj.name+'</option>'
     				$("#member").append(option)
                     $("#member_search").append(option)
     			}
     		}
+
 
     	});
         $( "#dialog-form" ).dialog({
@@ -52,3 +56,36 @@
             });
 
     })
+    function fill_accordion( date,name,fee,discription){
+        $( '#accordion' ).append('<h3>' + date + ' &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + name + '</h3>');
+        $( '#accordion' ).append('<div><p>金额：' + fee + '</p>'+'<p>' + discription + '</p></div>');
+
+    }
+    function ajaxSearch(){
+        $.post( "/" , {
+            start_time : $( '#start_time' ).val(),
+            end_time   : $( '#end_time').val(),
+            member_id  : $( '#member_id option:seleted').val()
+        },
+        function(result) {
+            if(result){
+                alert(result.length)
+                $( '#accordion' ).html('');
+                for(var i=0;i<result.length;i++){
+                    obj = result[i];
+
+                    fill_accordion(obj.consume_time,obj.name,obj.fee,obj.discription);
+                }
+                alert(1)
+                $( "#accordion" ).accordion({
+                    collapsible: true,
+                    heightStyle: "content"
+                });
+                alert(2)
+
+            }
+
+        });
+    }
+
+
